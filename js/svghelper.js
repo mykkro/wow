dc60cc07032ac405394aee6ec823ed9aa2c8a2a8
Myrousz <myrousz@gmail.com>
2014-01-06 08:06:40 +0100

    Added multiline text widget (currently uses foreignObject).

--- js/svghelper.js
+++ js/svghelper.js
@@ -2,6 +2,7 @@
 var SvgHelper = function(window) {
 	var document = window.document
 	var svgNS = "http://www.w3.org/2000/svg"
+	var htmlNS = "http://www.w3.org/1999/xhtml"
 	return {
 		box: function(bbox) {
 			if(!bbox) bbox = {}				
@@ -14,6 +15,14 @@ var SvgHelper = function(window) {
 			bbe.setAttribute("stroke", bbox.stroke || "black");  
 			return bbe;
 		},
+		mtext: function(text, options) {				
+		    var bbe = this.foreignObject(options)
+			if(options) this.attrs(bbe, options)
+			var p = document.createElementNS(htmlNS, "p")
+			p.textContent = text
+			bbe.appendChild(p)
+			return bbe;
+		},
 		group: function(options) {
 			var grp = document.createElementNS(svgNS, "g");
 			if(options) this.attrs(grp, options)
@@ -24,6 +33,15 @@ var SvgHelper = function(window) {
 			txt.textContent = text
 			if(options) this.attrs(txt, options)
 			return txt
+		},
+		foreignObject: function(bbox) {
+			if(!bbox) bbox = {}				
+		    var bbe = document.createElementNS(svgNS, "foreignObject");
+			bbe.setAttribute("x", bbox.x || 0);             
+			bbe.setAttribute("y", bbox.y || 0);
+			bbe.setAttribute("width", bbox.width || 10);         
+			bbe.setAttribute("height", bbox.height || 10);
+			return bbe;
 		},
 		attrs: function(node, attrs) {
 			for(var key in attrs) {
