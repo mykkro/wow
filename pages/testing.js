c0141a2afd4eebef7ae62a5e905f1d55f3c296fb
Myrousz <myrousz@gmail.com>
2014-01-08 16:49:33 +0100

    Page content now loaded via AJAX.

---
+++ pages/testing.js
@@ -0,0 +1,27 @@
+module.exports = function(window, $, SVG) {
+	var page = {
+		init: function(data, next) {
+			var draw = SVG('svg1')
+
+			// create image
+			var image = draw.image('icons/SVG/air.svg')		
+			$("#btn1").click(function() {
+			  var anim = image.animate(1000, SVG.easing.bounce).move(100, 100)
+			})
+		
+			// create text
+			var text = draw.text("Multiline\ntext").move(500, 0)
+			text.font({
+			  family: 'Source Sans Pro'
+			, weight: 300
+			, size: 30
+			, style: 'italic'
+			, anchor: 'middle'
+			, leading: '1em'
+			})
+			if(next) next(page)
+		}
+	}
+	return page
+
+}
