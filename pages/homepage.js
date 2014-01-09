9f855bc126b4eee94a1332308ffffa787b0fc7ad
Myrousz <myrousz@gmail.com>
2014-01-09 06:52:01 +0100

    Added updating of weather information.

--- pages/homepage.js
+++ pages/homepage.js
@@ -6,9 +6,45 @@ module.exports = function(window, $, SVG) {
 	// CalendarTextDayNo
 	// CalendarTextDayName
 
+	var weatherUri = "https://api.forecast.io/forecast/936df2f9a7b50fa0bd9006b85fdb9ece/48.14,16.20"
+	
+	
+	var fah2cel = function(temp) {
+		return (temp -32) * 5 / 9;
+	}
+	
+	var icon2char = function(icon) {
+		var map = {
+			"cloudy": "N",
+			"clear-day": "B",
+			"clear-night": "2",
+			"rain": "R",
+			"snow": "W",
+			"sleet": "X",
+			"wind": "S",
+			"fog": "L",
+			"partly-cloudy-day": "H",
+			"partly-cloudy-night": "4"
+		}
+		return map[icon] || "A"
+	}
+	
 	var timeinfo = require("../js/timeinfo")
 	var moment = require('moment');
 	moment.lang('de')
+	
+	function updateWeatherInfo() {
+		// get weather info...
+		$.getJSON(weatherUri).done(function(data) {
+		console.log(data)
+			var temp = Math.round(fah2cel(data.currently.temperature))
+			var icon = data.currently.icon
+			$("#MeteoIcon").text(icon2char(icon))
+			$("#MeteoTemperature").text(temp)
+			$("#MeteoUnit").text('*')
+		})
+
+	}
 	
 	function updateCalendar() {
 		console.log("Updating calendar...")
@@ -55,9 +91,11 @@ module.exports = function(window, $, SVG) {
 				/* use data to modify page */				
 				updateCalendar()
 				updateClock()
+				updateWeatherInfo()
 				// update calendar each minute...
 				window.setInterval(updateCalendar, 60*1000)
 				window.setInterval(updateClock, 1000)
+				window.setInterval(updateWeatherInfo, 10*60*1000)
 				/* continue when finished */
 				if(next) next(page)
 			})
