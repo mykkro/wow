af99aeeea395bb6c3a8dda5b0d16511790d6b935
Myrousz <myrousz@gmail.com>
2014-01-15 10:34:06 +0100

    Youtube search page now working.

--- pages/youtubepage.js
+++ pages/youtubepage.js
@@ -1,5 +1,5 @@
 module.exports = function(window, $, SVG) {
-
+	var url = require('url');	
 
 	var page = {
 		init: function(data, next) {
@@ -9,21 +9,63 @@ module.exports = function(window, $, SVG) {
 			var youtube = require('youtube-feeds')
 			var truncate = require('html-truncate');
 			var svgsvg = document.getElementById("svg")
+			var resultGrp
+			var leftBtn
+			var rightBtn
+			var textBox
 
+			/* update GUI with search results */
+			/* also update left/right button status */
 			var showSearchResults = function(err, data) {
 				console.log("Showing search results...")
 				if(err) console.error(err)
 				else {
+					if(!data) {
+						// show empty set...
+						data = {totalItems:0, startIndex:1, itemsPertPage:6, items:[]}
+					}
 					var total = data.totalItems
 					var start = data.startIndex
 					var pagesize = data.itemsPerPage
 					var items = data.items
 					console.log("Showing results "+start+"-"+(start+items.length-1)+" from "+total+" total")			
-					console.log(data)
-					for(var i=0; i<items.length; i++) {
-						showItem(items[i], i)
+					console.log(data)					
+					/* empty group... */
+					while( resultGrp.hasChildNodes() ){
+	    				resultGrp.removeChild(resultGrp.lastChild);
 					}
+					for(var i=0; i<6; i++) {
+						var item = null
+						if(i<items.length) item = items[i]
+						resultGrp.appendChild(showItem(item, i))
+					}
+					var leftEnabled = (start>1)
+					var rightEnabled = (start-1+items.length<total)
+					leftBtn.setEnabled(leftEnabled)
+					rightBtn.setEnabled(rightEnabled)
 				}
+			}
+
+			var getQueryString = function(dpage) {
+				var parsedUrl = url.parse(window.location.href, true)
+				parsedUrl.query.query = textBox.val()
+				var pg = parseInt(parsedUrl.query.page || 1)
+				if(dpage) pg += dpage
+				if(pg<1) pg=1
+				parsedUrl.query.page = pg
+				parsedUrl.search = null
+				return url.format(parsedUrl)
+			}
+
+
+			var goToPreviousPage = function() {
+				var href = getQueryString(-1)
+				window.location.href = href
+			}
+
+			var goToNextPage = function() {
+				var href = getQueryString(1)
+				window.location.href = href
 			}
 
 			var colors = ["#330099", "#f8c300", "#dd1379", "#dd1379", "#330099", "#f8c300"]
@@ -32,14 +74,28 @@ module.exports = function(window, $, SVG) {
 				var column = index%3
 				var row = Math.floor(index/3)	
 				var tx = 160 + column*223
-				var ty = 36 + row*223			
-				var label = truncate(data.title, 20)
-				var thumbUrl = data.thumbnail.sqDefault
-				var thumb = SvgHelper.image({x:7, y:20, width:180, height:120, src:thumbUrl})
+				var ty = 36 + row*223		
 				var rect = SvgHelper.rect({ry: 35, rx:35, height:195, width:195, fill:"#fff", stroke:colors[index], "stroke-width":5})
-				var txt = SvgHelper.text(label, {x:97, y: 170, "text-anchor":"middle"})
-				var grp = SvgHelper.group({"class":"youtube-results", transform: "translate("+tx+", "+ty+")"}, [rect, thumb, txt])
-				svgsvg.appendChild(grp)
+				var items = [rect]
+				var klass = "youtube-result"
+				if(data) {
+					var label = truncate(data.title, 20)
+					var thumbUrl = data.thumbnail.sqDefault
+					var thumb = SvgHelper.image({x:7, y:20, width:180, height:120, src:thumbUrl})
+					var txt = SvgHelper.text(label, {x:97, y: 170, "text-anchor":"middle"})
+					items = [rect, thumb, txt]
+					$(thumb).click(function() {
+						// go to video page...
+						goToVideoPage(data.id)
+					})
+				} else {
+					klass += " disabled"
+				}
+				return SvgHelper.group({"class":klass, transform: "translate("+tx+", "+ty+")"}, items)
+			}
+
+			var goToVideoPage = function(ytId) {
+				window.location.href = "gui.html?view=videopage&id="+ytId
 			}
 
 			var searchYouTubeVideos = function(data, next) {
@@ -64,7 +120,19 @@ module.exports = function(window, $, SVG) {
 			var node = window.document
 			Widgetizer.widgetize(node, function() {
 				/* widgetization complete! */
+				resultGrp = SvgHelper.group({"class":"youtube-results"})
+				svgsvg.appendChild(resultGrp)
+				leftBtn = Widgetizer.get("leftButton")
+				rightBtn = Widgetizer.get("rightButton")	
+				textBox = Widgetizer.get("searchTextbox")
 
+				// TODO buttons work even when they are disabled... why?
+				leftBtn.click(goToPreviousPage)
+				rightBtn.click(goToNextPage)
+
+				showSearchResults()
+
+				/* bind events... */
 				var quitBtn = Widgetizer.get("quitButton")
 				quitBtn.click(function() {
 					// move back to previous page...
@@ -77,10 +145,10 @@ module.exports = function(window, $, SVG) {
 				var query = data.query.query
 				var page = parseInt(data.query.page || 1)
 				if(query) {
-					Widgetizer.get("searchTextbox").val(query)				
+					textBox.val(query)				
 					searchIt(page)
 				}
-				Widgetizer.get("searchTextbox").onEnterPressed = function() {
+				textBox.onEnterPressed = function() {
 					searchIt(page)
 				}
 				Widgetizer.get("searchButton").click(function() {
