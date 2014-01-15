62ef3189b2eb74da9bce84d34967254bdce08b16
Myrousz <myrousz@gmail.com>
2014-01-15 12:37:29 +0100

    Added modal dialog example.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ grunt.initConfig({
         mac: false, 
         win: true, // We want to build it for win
         linux32: false, // We don't need linux32
-        linux64: false, // We don't need linux64
+        linux64: true, // We don't need linux64
 		version: '0.8.4'
     },
     src: ['./**/*'] // Your node-wekit app
