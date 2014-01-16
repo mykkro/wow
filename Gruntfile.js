06b3e686bdda1f18eed7c2a43218a4445204000c
Myrousz <myrousz@gmail.com>
2014-01-16 11:49:19 +0100

    ...

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,14 @@ module.exports = function (grunt) {
 grunt.initConfig({
   nodewebkit: {
     options: {
-        build_dir: '../webkitbuilds', // Where the build version of my node-webkit app is saved
+        build_dir: 'webkitbuilds', // Where the build version of my node-webkit app is saved
         mac: false, 
         win: true, // We want to build it for win
         linux32: false, // We don't need linux32
         linux64: true, // We don't need linux64
-		version: '0.8.4'
+		version: '0.8.4',
+        app_name: 'wow',
+        app_version: '0.1'
     },
     src: ['./**/*'] // Your node-wekit app
   },
