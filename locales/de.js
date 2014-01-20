c656b0726f860367994e7fd1b1ea35a13e09ea81
Myrousz <myrousz@gmail.com>
2014-01-20 13:05:06 +0100

    Uses styled modal dialog for quit confirmation.

--- locales/de.js
+++ locales/de.js
@@ -4,5 +4,9 @@
 	"midday": "mittag",
 	"This will quit the application. Are you sure?": "Beenden?",
 	"morning": "vormittag",
-	"dynamic string": "dynamic string"
+	"dynamic string": "dynamic string",
+	"Exit": "Beenden",
+	"Are you sure?": "Sind Sie sicher?",
+	"Yes": "Ja",
+	"No": "Nein"
 }
