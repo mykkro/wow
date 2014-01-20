a87d6b281b8bfb9d4e708b5855f221a309a86912
Myrousz <myrousz@gmail.com>
2014-01-20 13:39:12 +0100

    Simple login page added.

--- pages/loginpage.js
+++ pages/loginpage.js
@@ -1,12 +1,25 @@
 module.exports = function(window, $, SVG, i18n) {
+
+	function tzLogin() {
+		//window.alert("TZ!")
+		window.location = baseUrl + "?view=homepage"
+	}
+
+	function adminLogin() {
+		//window.alert("Admin!")
+	}
+	var baseUrl;
 	var page = {
 		init: function(data, next) {
+			baseUrl = data.baseUrl
 			var Widgetizer = require("../js/widgetizer")(window, $)
 			/* load basic widgets used by this page... */
 			Widgetizer.useCommonWidgets()
 			/* transform wow:markup to SVG and widgets */
 			Widgetizer.widgetize(window.document, function() {
 				/* do something... */
+				$("[name=tzlogin] .overlay").click(tzLogin)
+				$("[name=adminlogin] .overlay").click(adminLogin)
 
 				/* continue when finished */
 				if(next) next(page)
