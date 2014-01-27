a4473752a796c0b54ce10f313437de671ac2414d
Myrousz <myrousz@gmail.com>
2014-01-27 12:32:52 +0100

    Experiment with JSON RPC under Express.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* wow server */
 
 var express = require('express');
+//var jsonrpc = require('node-express-json-rpc2')
 var routescan = require('express-routescan');	
 var path = require("path")
 var storage = require("./lib/storage")
@@ -8,13 +9,44 @@ var storage = require("./lib/storage")
 var WowServer = {
 	port: 9999,
 	start: function() {
-		var app = express();
+		var app = express()
 		var currentDir = process.cwd() // __dirname does not work in node-webkit
 
-		routescan(app)
-		app.use("/imports", express.static(storage.importDir))
-		app.use(express.static(currentDir + '/public'));
 
+        app.configure(function(){
+            //app.use( express.jsonrpc() );
+    		routescan(app)
+	        app.use("/imports", express.static(storage.importDir))
+	        app.use(express.static(currentDir + '/public'));
+            app.use( app.router );
+        });
+/*
+        app.post('/rpc', function(req, res, next){
+            // notification (no response expected)
+            res.rpc('notification_method', function (params) {
+                // do processing here
+            });
+
+            // non-notification (response expected)
+            res.rpc('method_name', function(params, respond){
+                // do processing here
+
+                // if everything is OK return result object:
+                respond({ result: 666 });
+
+                // if something is wrong, return an error code:
+                respond(jsonrpc.INVALID_PARAMS)
+                // OR an extended error object:
+                respond({
+                    error: {
+                        code: jsonrpc.INVALID_PARAMS,
+                        message: 'You gave me invalid parameters!',
+                        data: []
+                    }
+                });
+            });
+        });
+*/
 		app.listen(this.port);
 		console.log('Listening on port '+this.port);
 	}
