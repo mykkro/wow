cbef1773eebf241d9cac519b4958083155217dcd
Myrousz <myrousz@gmail.com>
2014-01-27 13:25:58 +0100

    Added server local call method with the same method signature as RPC call method.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var routescan = require('express-routescan');
 var path = require("path")
 var storage = require("./lib/storage")
 var rpcMethods = require('./lib/rpc/methods');
+var http = require('http');
 
 var WowServer = {
 	port: 9999,
@@ -71,7 +72,7 @@ var WowServer = {
           } catch (e) {
             onError({
               code: -32603,
-              message: 'Excaption at method call',
+              message: 'Exception at method call',
               data: e
             }, 500);
           }
@@ -88,7 +89,56 @@ var WowServer = {
 
 		app.listen(this.port);
 		console.log('Listening on port '+this.port);
-	}
+
+	},
+    /* Local call */
+    // TODO:return values should have the same structure as in remote calls... */
+    loc: function(method, params, next) {
+        if(rpcMethods[method]) {
+            rpcMethods[method](params, function(err, res) {
+                if(err) next({message:'Failed', data:error});
+                else next(null, {result:res, error: null})
+            })
+        } else {
+            next({message:"Method not found"})
+        }
+    },
+    /* Remote call */
+    rpc: function(method, params, next) {
+        var rpcObj = {"jsonrpc":"2.0", method:method, params: params}
+        var rpcStr = JSON.stringify(rpcObj)
+        var headers = {
+          'Content-Type': 'application/json',
+          'Content-Length': rpcStr.length
+        };
+        var options = {
+          host: 'localhost',
+          port: this.port,
+          path: '/rpc',
+          method: 'POST',
+          headers: headers
+        };
+
+        // Setup the request.  The options parameter is
+        // the object we defined above.
+        var req = http.request(options, function(res) {
+          res.setEncoding('utf-8');
+          var responseString = '';
+          res.on('data', function(data) {
+            responseString += data;
+          });
+          res.on('end', function() {
+            var resultObject = JSON.parse(responseString);
+            next(null, resultObject)
+          });
+        });
+        req.on('error', function(e) {
+          next(e)
+        });
+        req.write(rpcStr);
+        req.end();
+
+    }
 
 }
 
