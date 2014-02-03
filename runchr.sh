aec806fe7396328a3f1fc4e557654a3359387753
Myrousz <myrousz@gmail.com>
2014-02-03 16:27:26 +0100

    Option --kiosk does not work with --window-size.

--- runchr.sh
+++ runchr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 node runserver.js &
-chromium-browser --kiosk --window-size=960,600 --url http://localhost:9999 --no-proxy-server
+chromium-browser --kiosk --url http://localhost:9999 --no-proxy-server
 wait
 
