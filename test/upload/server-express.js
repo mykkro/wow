7210bb8cc4309775504d141d6c6d9f60157407f4
Myrousz <myrousz@gmail.com>
2014-02-05 20:15:46 +0100

    Some experiments with blueimp jquery file upload middleware.

---
+++ test/upload/server-express.js
@@ -0,0 +1,43 @@
+// TODO POST not working...
+
+'use strict';
+
+var express = require('express');
+var upload = require('jquery-file-upload-middleware');
+
+var app = express()
+
+    // configure upload middleware
+    upload.configure({
+        uploadDir: __dirname + '/public/files',
+        uploadUrl: '/uploads',
+        imageVersions: {
+            thumbnail: {
+                width: 80,
+                height: 80
+            }
+        }
+    });
+    upload.on('end', function (fileInfo) {
+        console.log("File uploaded!")
+        console.log(fileInfo)
+    });
+
+app.configure(function() {
+  app.set("view options", {layout: false});
+  app.use('/fileupload', upload.fileHandler());
+  // express bodyparser must be AFTER the fileupload middleware!
+  // or, more exactly, fileupload must be BEFORE the other middlewares 
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use("/js", express.static("./js"))
+  app.use("/public", express.static("./public"))
+  app.use("/uploads", express.static("./public/files"))
+  app.get('/', function(req, res) {
+    res.sendfile('index-express.html');
+  });
+  app.listen(9999)
+})
+
+
+
