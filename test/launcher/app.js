5ad6993fe4b6e083da0d9ddf04b7721dc02f5cd1
Myrousz <myrousz@gmail.com>
2014-02-05 10:21:28 +0100

    Started node-launcher test module.

---
+++ test/launcher/app.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var nc = require('node-chrome');
+
+// chromium-browser --kiosk --url http://localhost:9999 --no-proxy-server
+
+var opts = {
+  runtime: "chromium-browser",
+  files: "./ui",
+  port: 8080,
+  index: "/index.html",
+  width: 1024,
+  height: 760
+};
+
+nc(opts, function(websocket, chrome) {
+
+  // output from the socket.
+  websocket.on('message', function(message) {
+    console.log(message);
+  });
+
+  // output from the chrome runtime.
+  chrome.stdout.on('data', function (data) {
+    console.log(data);
+  });
+
+  // when the user quits the app.
+  chrome.on('exit', function (code) {
+    process.exit(0);
+  });
+});
