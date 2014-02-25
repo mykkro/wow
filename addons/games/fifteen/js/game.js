82064f534a1a0425f8d872e56af5abbac1c26711
Myrousz <myrousz@gmail.com>
2014-02-25 15:15:42 +0100

    ...

--- addons/games/fifteen/js/game.js
+++ addons/games/fifteen/js/game.js
@@ -18,3 +18,105 @@ var Game = Base.extend({
     if(cb) cb()
   }
 })
+
+var Splash = function(options) {
+  var div = $("<div>").addClass("splash").text(options.text)
+  div.appendTo($("body"))
+  setTimeout(function() {
+    div.remove()
+    if(options.after) options.after()
+  }, options.delay || 1000)
+}
+
+
+// UI - put into a class...
+
+function showTab(name) {
+  $(".tabs .tab").hide()
+  $("#tab-"+name).show()
+
+}
+
+function updateUI(playing) {
+  $("button.game-new").prop("disabled", playing)
+  $("button.game-quit").prop("disabled", !playing)
+  $("button.game-restart").prop("disabled", !playing)
+  $("button.game-settings").prop("disabled", playing)
+  $("button.game-rules").prop("disabled", playing)
+  $("button.game-scores").prop("disabled", playing)
+  $("button.game-info").prop("disabled", playing)
+}
+
+
+function runGame(game) {
+
+    function startGame() {
+      // start game...
+      alertify.success("Starting game...");
+      Splash({
+        text:"Starting game...",
+        delay: 2000,
+        overlay: true,
+        after: function() {
+          game.start(function() {
+            playing = true;
+            updateUI(playing)
+            showTab("game")
+          })
+        }
+      })
+
+    }
+
+    function quitGame() {
+      // quit game...
+      game.quit(function() {
+        playing = false;
+        updateUI(playing)
+        showTab("info")
+      })
+    }
+
+    function restartGame() {
+      game.restart(function() {
+        playing = true;
+        updateUI(playing)
+        showTab("game")
+      
+      })
+    }
+
+    function showGameInfo() {
+      showTab("info")
+    }
+
+    function showGameRules() {
+      showTab("rules")
+    }
+
+    function showGameScores() {
+      showTab("scores")
+    }
+
+    function gameSettings() {
+      showTab("settings")
+    }
+
+    // initialization...
+    game.init(function() {
+      console.log("Game initialized!")
+      var playing = false
+      updateUI(playing)
+      showGameInfo()
+
+      $("button.game-new").click(startGame)
+      $("button.game-quit").click(quitGame)
+      $("button.game-restart").click(restartGame)
+      $("button.game-info").click(showGameInfo)
+      $("button.game-rules").click(showGameRules)
+      $("button.game-scores").click(showGameScores)
+      $("button.game-settings").click(gameSettings)
+
+      $(".container").zoomTo({targetsize: 1, duration:1});
+    })
+}
