ade1603cb3914663883aafdc57c1f95f8f729da5
Myrousz <myrousz@gmail.com>
2014-02-27 00:13:25 +0100

    Unified display of game information (scores, lives etc.).

--- addons/games/raphanoid/js/game.js
+++ addons/games/raphanoid/js/game.js
@@ -1,27 +1,49 @@
 var Game = Base.extend({
   // options
-
+  constructor: function(options) {
+    this.options = options || {}
+  },
   init: function(cb) {
     // do some initialization...
+    this.log("status", "initialize")
     if(cb) cb()
   },
   start: function(cb) {
     // start the game...
+    this.log("status", "start")
     if(cb) cb()
   },
   restart: function(cb) {
     // restart game
+    this.log("status", "restart")
     var self = this
     this.stop(function() {
       self.start(cb)  
     })
     
   },
-  stop: function() {
+  stop: function(cb) {
+    this.log("status", "stop")
     if(cb) cb()
   },
   quit: function(cb) {
+    this.log("status", "quit")
     this.stop(cb)
+  },
+  availableLogs: function() {
+    return {
+      // map of available logs
+      status: { "label": "Status", "type": "string", "defaultValue": "", "visible": false },
+      score: { "label": "Score", "type": "integer", "defaultValue": 0, "visible": true }
+    }
+  },
+  log: function(name, value) {
+    if(this.options.logger) {
+      this.options.logger(name, value)
+    }
+  },
+  setLogger: function(logger) {
+    this.options.logger = logger
   }
 }, {
   prompt: function(text, options, cb) {
