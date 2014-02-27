68c7c0bf913f16241be045adc17b0c10f5c49219
Myrousz <myrousz@gmail.com>
2014-02-27 01:19:17 +0100

    Added score counter for Raphanoid.

--- addons/games/raphanoid/js/gameui.js
+++ addons/games/raphanoid/js/gameui.js
@@ -1,3 +1,45 @@
+var MiniLog = Base.extend({
+  constructor: function(name, logInfo) {
+      this.info = logInfo
+      this.name = name
+      this.element = $("<div>").addClass("log")
+      if(!logInfo.visible) this.element.addClass("hidden")
+      this.init()
+      this.element.append(
+        $("<span>").addClass("label").text(this.info.label+": "),
+        this.valueElement
+      )      
+      this.update(logInfo.defaultValue)
+  },
+  init: function() {
+      this.valueElement = $("<span>").addClass(this.name)
+  },
+  update: function(value) {
+     this.valueElement.text(value)
+  }
+}, {
+  create: function(name, logInfo) {
+    if(name=="lives") {
+      return new LivesMiniLog(name, logInfo)
+    }
+    return new MiniLog(name, logInfo)
+  }
+})
+
+var LivesMiniLog = MiniLog.extend({
+  constructor: function(name, logInfo) {
+    this.base(name, logInfo)
+  },
+  init: function() {
+    this.valueElement = $('<div class="lifebar-wrapper"><div class="lifebar"></div></div>')
+  },
+  update: function(value) {
+    console.log("LivesMiniLog!")
+    var innerWidth = Math.round(value*32) // size of the heart icon
+    this.valueElement.find(".lifebar").css("width", innerWidth)
+  }
+})
+
 var GameUI = Base.extend({
   constructor: function(game) {
     this.game = game
@@ -6,26 +48,20 @@ var GameUI = Base.extend({
     game.setLogger(function(name, value) {
       self.updateLogs(name, value)
     })
-    this.initLogs()
+    this.logs = {}
+    this.initLogs()    
   },
   initLogs: function() {
-//    Score: <span class="score"></span> Lives: <span class="lives"></span>
     var logs = this.game.availableLogs()
     var cont = $(".container .info .logs")
     for(var name in logs) {      
-      console.log(name+"="+logs[name].defaultValue)
-      var div = $("<div>").addClass("log").appendTo(cont)
-      if(!logs[name].visible) div.addClass("hidden")
-      div.append($("<span>").addClass("label").text(logs[name].label+": "))
-      div.append($("<span>").addClass(name))
-      // very crude view...
-      $(".container .info .logs ."+name).text(logs[name].defaultValue)
+      this.logs[name] = MiniLog.create(name, logs[name])
+      cont.append(this.logs[name].element)
     }
   },
   updateLogs: function(name, value) {
     console.log(name+"="+value)
-    // very crude view...
-    $(".container .info .logs ."+name).text(value)
+    this.logs[name].update(value)
   },
   showTab: function(name) {
     $(".tabs .tab").hide()
--- addons/games/raphanoid/js/raphanoid/scorecounter.js
+++ addons/games/raphanoid/js/raphanoid/scorecounter.js
@@ -1,15 +1,17 @@
 Raphanoid.ScoreCounter = Raphanoid.Komponent.extend({
 	constructor: function(paper) {
 		this.base(paper);
+        /*
         this.element = paper.text(200, 8).attr({
             "font-size": "16",
             "font-weight": "bold"
         });
+        */
 		this.reset();
     },
     setScore: function (score) {
         this.score = score;
-        this.element.attr("text", "Score: " + score);
+        //this.element.attr("text", "Score: " + score);
     },
     reset: function (score) {
         this.setScore(0);
