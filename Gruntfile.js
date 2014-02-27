0f107a3c6cfea3f7f7acfb5127e4a8e8fa701898
Myrousz <myrousz@gmail.com>
2014-02-27 09:24:24 +0100

    ..

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,7 +132,7 @@ module.exports = function (grunt) {
         main: {
           options: {
             // TODO how to get this filename by appending version number from package.json?
-            archive: 'dist/wow-0.6.zip'
+            archive: 'dist/wow-0.6.1.zip'
           },
           files: [
             {src: ['assets/**', 'css/**', 'fonts/**', 'js/**', 'lib/**', 'locales/**', 'media/**', 'pages/**', 'public/**', 'routes/**', 'templates/**', 'views/**']},
