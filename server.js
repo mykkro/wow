85aea7e227e8784081d2b24f5d967fa24bfa4f5a
Myrousz <myrousz@gmail.com>
2014-03-19 02:04:02 +0100

    Refactoring of page markup.

--- server.js
+++ server.js
@@ -117,13 +117,22 @@ var WowServer = {
 
         app.get('/pages/:name', function(req, res) {
           var name = req.params.name
-          var view = fs.readFileSync("pages/"+name+"/index.wow", "utf8")
-          var defs = fs.readFileSync("public/assets/misc/defs", "utf8")
-          var viewData = {query:req.query, defs:defs}
-          var viewHtml = mustache.to_html(view, viewData)
           var page = fs.readFileSync("templates/master.html", "utf8")
-          var data = {query:req.query, "name":name, "content":viewHtml}
-          var html = mustache.to_html(page, data); 
+          var defs = fs.readFileSync("public/assets/misc/defs", "utf8")
+          if(fs.existsSync("pages/"+name+"/index.wow")) {
+            // page is internal SVG content
+            var view = fs.readFileSync("pages/"+name+"/index.wow", "utf8")
+            var master = fs.readFileSync("templates/page.svg", "utf8")
+            var viewSVG = mustache.to_html(master, {defs:defs, content:view})
+            var viewHtml = mustache.to_html(viewSVG, {query:req.query})
+            var html = mustache.to_html(page, {query:req.query, "name":name, "content":viewHtml}); 
+          } else {
+            // page is already a wrapped html page
+            var view = fs.readFileSync("pages/"+name+"/index.html", "utf8")
+            var viewSVG = mustache.to_html(view, {defs:defs, query:req.query})
+            var viewHtml = viewSVG
+            var html = mustache.to_html(page, {query:req.query, "name":name, "content":viewHtml});
+          }
           res.send(html)
         })
 /*
