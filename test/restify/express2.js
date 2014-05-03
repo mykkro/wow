ab88ff691c7c938631b4e6eb7af98f80d1782a25
Myrousz <myrousz@gmail.com>
2014-05-03 14:40:13 +0200

    Working REST token authentication example (in test/jwt).

--- test/restify/express2.js
+++ test/restify/express2.js
@@ -10,55 +10,12 @@ var app = express();
 app.configure(function(){
    	app.use(express.json());
 	app.use(express.urlencoded());   
+	app.use(express.methodOverride());
+    app.use(express.cookieParser());
 });
  
-// API functions...
-var API = {
-	getEmployee: function(name) {
-		var employee = new Object();
-		employee.name = name;
-		employee.age = 25;
-		employee.deparment = "HR";
-		employee.wage = 15000.00;	
-		
-		address = new Object();
-		address.city = "Massachusetts";
-		address.state = "Springfield";
-		address.street = "Evergreen";
-		address.zip = 66450;
-		
-		employee.address = address;
-		return employee
-	},
-	createEmployee: function(data) {
-		/* does nothing */
-		return { "params": data }
-	},
-	createNamedEmployee: function(name, data) {
-		/* does nothing */
-		return { "name": name, "params": data }
-	},
-	deleteEmployee: function(name) {
-		/* does nothing */
-		return { "deleted": name }
-	}
-}
+var API = require("./EmployeeAPI")
 
-app.get('/employee/getEmployee/:name', function(req, res) {
-  	res.type('application/json');
-  	var name = req.params.name;
- 	var out = API.getEmployee(name)	
-	res.json(out);
-});
- 
-app.post('/employee/postEmployee', function(req, res) {
-	var data = req.body;
-	console.log("Got request: " + JSON.stringify(data));
-	var out = API.createEmployee(data)
-	res.send(out);		
-});	
-
-/* shorthand more RESTful api */
 app.get('/empl/:name', function(req, res) {
   	res.type('application/json');
   	var name = req.params.name;
@@ -89,15 +46,11 @@ app.post('/empl/new', function(req, res) {
 app.listen(process.env.PORT || 9999);
 
 // Test it:
-// curl -X GET localhost:9999/employee/getEmployee/mykkro
-// curl -d '{"hello": "world"}' -H "Content-Type: application/json" localhost:9999/employee/postEmployee
-
-// shortened API (more restful)
 // GET:    
 // 	curl -X GET localhost:9999/empl/mykkrousz
 // DELETE: 
 // 	curl -X DELETE localhost:9999/empl/mykkrousz
 // POST:   
-// 	curl -d '{"hello": "world"}' -H "Content-Type: application/json" localhost:9999/empl/new
+// 	curl -X POST -d '{"hello": "world"}' -H "Content-Type: application/json" localhost:9999/empl/new
 // PUT:    
-// 	 curl -X PUT -d '{"hello": "world"}' -H "Content-Type: application/json" localhost:9999/empl/mykkrousz
+// 	curl -X PUT -d '{"hello": "world"}' -H "Content-Type: application/json" localhost:9999/empl/mykkrousz
