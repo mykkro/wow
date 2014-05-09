6bfc2b19aa508bc29082b4bff127133ebf41dad0
Myrousz <myrousz@gmail.com>
2014-05-09 13:43:20 +0200

    Tested converting of some videos with ffmpeg2theora.

---
+++ test/videoconvert/convert-video.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# convert MP4 file
+ffmpeg2theora ../../tools/craft/media/test.mp4 -o output/test.mp4.ogv 
+
+# convert AVI file
+ffmpeg2theora ../../tools/craft/media/test.avi -o output/test.avi.ogv 
+
+# convert MOV file
+ffmpeg2theora ../../tools/craft/media/test.mov -o output/test.mov.ogv 
+
+# convert MPEG file
+ffmpeg2theora ../../tools/craft/media/test.mpg -o output/test.mpg.ogv 
+
+# convert 3GP file
+ffmpeg2theora ../../tools/craft/media/test.3gp -o output/test.3gp.ogv 
+
+# convert FLV file
+ffmpeg2theora ../../tools/craft/media/test.flv -o output/test.flv.ogv 
+
+# convert WEBM file
+ffmpeg2theora ../../tools/craft/media/test.webm -o output/test.webm.ogv 
+
