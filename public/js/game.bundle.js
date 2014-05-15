c81f6205422295dedb6a22c0960c0fb7cc7519ef
Myrousz <myrousz@gmail.com>
2014-05-15 08:25:40 +0200

    Tried to get YouTube metadata by search API, but due to some problem the superclass .create method in YouTubeAPI is never called when invoked from callback function.

--- public/js/game.bundle.js
+++ public/js/game.bundle.js
@@ -1,4 +1,6 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"/9Q6Zj":[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"Game":[function(require,module,exports){
+module.exports=require('sT/mJt');
+},{}],"sT/mJt":[function(require,module,exports){
 "use strict";
 
 var Base = require("basejs")
@@ -123,9 +125,7 @@ var Game = Base.extend({
 
 module.exports = Game
 
-},{"./Splash":7,"basejs":12}],"Game":[function(require,module,exports){
-module.exports=require('/9Q6Zj');
-},{}],"Qz+ab4":[function(require,module,exports){
+},{"./Splash":7,"basejs":12}],"aYzOZh":[function(require,module,exports){
 var Base = require("basejs")
 
 var GridController = Base.extend({
@@ -186,8 +186,8 @@ var GridController = Base.extend({
 module.exports = GridController
 
 },{"basejs":12}],"GridController":[function(require,module,exports){
-module.exports=require('Qz+ab4');
-},{}],"0D1Aa+":[function(require,module,exports){
+module.exports=require('aYzOZh');
+},{}],"EQA1QC":[function(require,module,exports){
 "use strict";
 
 var Base = require("basejs")
@@ -237,7 +237,7 @@ var LivesMiniLog = MiniLog.extend({
 module.exports = MiniLog
 
 },{"basejs":12}],"MiniLog":[function(require,module,exports){
-module.exports=require('0D1Aa+');
+module.exports=require('EQA1QC');
 },{}],7:[function(require,module,exports){
 "use strict";
 
@@ -277,7 +277,9 @@ Splash.removeAll = function() {
 
 module.exports = Splash
 
-},{}],"8RkSAW":[function(require,module,exports){
+},{}],"Util":[function(require,module,exports){
+module.exports=require('TyP1pn');
+},{}],"TyP1pn":[function(require,module,exports){
 "use strict";
 
 var Util = {
@@ -317,9 +319,9 @@ var Util = {
 
 module.exports = Util
 
-},{}],"Util":[function(require,module,exports){
-module.exports=require('8RkSAW');
-},{}],"JnjsU8":[function(require,module,exports){
+},{}],"Watches":[function(require,module,exports){
+module.exports=require('wKKN3g');
+},{}],"wKKN3g":[function(require,module,exports){
 "use strict";
 
 var Base = require("basejs")
@@ -349,9 +351,7 @@ var Watches = Base.extend({
 
 module.exports = Watches
 
-},{"basejs":12}],"Watches":[function(require,module,exports){
-module.exports=require('JnjsU8');
-},{}],12:[function(require,module,exports){
+},{"basejs":12}],12:[function(require,module,exports){
 /*
   Based on Base.js 1.1a (c) 2006-2010, Dean Edwards
   Updated to pass JSHint and converted into a module by Kenneth Powers
@@ -498,4 +498,4 @@ module.exports=require('JnjsU8');
   return Base;
 });
 
-},{}]},{},["/9Q6Zj","Qz+ab4","0D1Aa+","8RkSAW","JnjsU8"])
+},{}]},{},["sT/mJt","aYzOZh","EQA1QC","TyP1pn","wKKN3g"])
--- tools/craft/Gruntfile.js
+++ tools/craft/Gruntfile.js
@@ -23,6 +23,7 @@ module.exports = function(grunt) {
           main: {
             files: [
               {expand: true, cwd: 'files', src: 'Storage.js', dest: 'output/lib'},
+              {expand: true, cwd: 'files', src: 'YouTubeUtil.js', dest: 'output/lib'},
               {expand: true, cwd: 'files', src: 'NodeDAO.js', dest: 'output/lib/dao'},
               {expand: true, cwd: 'files', src: 'NodeAPI.js', dest: 'output/lib/api'},
               {expand: true, cwd: 'files', src: 'IndexedNodeAPI.js', dest: 'output/lib/api'},
