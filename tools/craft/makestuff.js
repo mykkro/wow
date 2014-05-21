26f01b300f8459fdf4aeba1918c9e62a3333b6cf
Myrousz <myrousz@gmail.com>
2014-05-21 20:06:25 +0200

    ...

--- tools/craft/makestuff.js
+++ tools/craft/makestuff.js
@@ -30,30 +30,38 @@ var theme = {
 var theme = tapi.createp(theme)
 
 // create some links
-// // TODO this creates links with the same ID!
-var link1 = sapi.createp({title:"Books", uri:"/pages/bookcategories"})
-var link2 = sapi.createp({title:"Radio", uri:"/pages/netradio"})
-var link3 = sapi.createp({title:"Apps", uri:"/pages/apps"})
-var link4 = sapi.createp({title:"YouTube", uri:"/pages/searchvideos"})
+var lll = []
+sapi.createp({title:"Books", uri:"/pages/bookcategories"}).done(function(l1) {
+	lll.push(l1[0])
+	sapi.createp({title:"Radio", uri:"/pages/netradio"}).done(function(l2) {
+		lll.push(l2[0])
+		sapi.createp({title:"Apps", uri:"/pages/apps"}).done(function(l3) {
+			lll.push(l3[0])
+			sapi.createp({title:"YouTube", uri:"/pages/searchvideos"}).done(function(l4) {
+				lll.push(l4[0])
 
-// TODO I need those to execute in sequence
-var links = Q.all([link1, link2, link3, link4])
+				Q.all([loc, theme]).done(function(data) {
+					console.log(data)
+					var ll = data[0][0]
+					var theme = data[1][0]
+					var links = lll
+					// create a preset... it has links!
+					var preset = {
+						title: "Default preset",
+						"button1LinkId": parseInt(links[0]._id),
+						"button2LinkId": parseInt(links[1]._id),
+						"button3LinkId": parseInt(links[2]._id),
+						"themeId": parseInt(theme._id)
+					}
+					papi.createp(preset).done(console.log)
+				})
 
 
-Q.all([loc, theme, links]).done(function(data) {
-	console.log(data)
-	var ll = data[0][0]
-	var theme = data[1][0]
-	var links = _.map(data[2], function(x) { return x[0] })
-	console.log(ll, theme, links)
-	// create a preset... it has links!
-	var preset = {
-		title: "Default preset",
-		"button1LinkId": parseInt(links[0]._id),
-		"button2LinkId": parseInt(links[1]._id),
-		"button3LinkId": parseInt(links[2]._id),
-		"themeId": parseInt(theme._id)
-	}
-	papi.createp(preset).done(console.log)
+			})
+		})
+	})
 })
+
+
+
 
