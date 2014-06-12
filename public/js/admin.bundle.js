539b56b8e63f897ea594f9ddfb9da76f65df6be9
Myrousz <myrousz@gmail.com>
2014-06-12 15:54:40 +0200

    Replaced i18n-2 by i18next also in user/plugin pages.

--- public/js/admin.bundle.js
+++ public/js/admin.bundle.js
@@ -1,4 +1,6 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"7oXSFR":[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"AdminPage":[function(require,module,exports){
+module.exports=require('7oXSFR');
+},{}],"7oXSFR":[function(require,module,exports){
 var AdminPage = function($, i18n, pageMode, editableNodes, node, nodeType) {
 
     $.playable('soundmanager/swf/')
@@ -21,7 +23,6 @@ var AdminPage = function($, i18n, pageMode, editableNodes, node, nodeType) {
     var Commons = require("./Commons")
 
     var doAjax = Commons.doAjax
-
 
     var putImage = function(dropData, next) {
         console.log("Store image to DB", dropData)
@@ -549,17 +550,15 @@ _.each(editableNodes, function(en) {
         })
     }
 
-    /**/
+    /*
     showItemSelector(["image"], function(data) {
         console.log("Selected:", data)
     })
-    /**/
+    */
 }
 
 module.exports = AdminPage
-},{"./Commons":3,"./Dialogs":4,"./Widgetizer":9}],"AdminPage":[function(require,module,exports){
-module.exports=require('7oXSFR');
-},{}],3:[function(require,module,exports){
+},{"./Commons":3,"./Dialogs":4,"./Widgetizer":9}],3:[function(require,module,exports){
 // Commons.js
 
 // commonly used functions.
