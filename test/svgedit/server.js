e113a849746c09397c19d24cb00d38bae8ba83e5
Myrousz <myrousz@gmail.com>
2014-06-17 18:14:52 +0200

    Added Method Draw editor, to be embedded to SVG create/edit page.

---
+++ test/svgedit/server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var path = require("path")
+var http = require('http');
+var express = require('express')
+var app = express()
+var server = http.createServer(app)
+
+
+app.set("view options", {layout: false});
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js',express.static(path.join(__dirname, '../../js')));
+
+var port = 9999
+server.listen(port, function() {
+  console.log('Listening on port '+port);
+});
+  
