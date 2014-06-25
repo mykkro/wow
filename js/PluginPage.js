e736c355d4cfe7e62e38fdefe7811b7a09b1f535
Myrousz <myrousz@gmail.com>
2014-06-25 09:57:15 +0200

    Minor changes.

---
+++ js/PluginPage.js
@@ -0,0 +1,9 @@
+var PluginPage = function($, i18n, SVG, moment, window, currentLocale, location, preset) {
+
+    var document = window.document
+
+    // TODO add code from views/mustache/page-master here...
+
+}
+
+module.exports = PluginPage
