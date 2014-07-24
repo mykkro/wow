369af70b7c341911c960d6c30d0894772b44d8ac
Myrousz <myrousz@gmail.com>
2014-07-24 12:14:29 +0200

    Minor fix in itemlists of favorite/personal items.

--- plugins/youtubepersonal/src/main.js
+++ plugins/youtubepersonal/src/main.js
@@ -28,8 +28,7 @@ module.exports = function(Wow) {
             var skip = ((page-1)*6)
             $.getJSON("/api/youTubeVideo/personal?skip="+skip+"&limit=6").done(function(items) {
                 console.log("Found items: ", items)
-                var total = items.length
-                if(total==6) total=7
+                var total = skip + items.length + 1
                 var obj = {totalItems: total, startIndex: 1+skip, itemsPerPage: 6, items: items} 
                 self.displayResults(page, obj, next)
             })
